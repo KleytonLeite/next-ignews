3b22ed702676241591fa696cd9c58f027d82f8d1
KleytonLeite <musicpteclados@gmail.com>
2021-04-03 11:39:56 -0300

    feat: Estruturando e stylizando pagina home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,27 @@
 import Head from 'next/head';
 
+import styles from './home.module.scss';
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏Hey, welcome</span>
+          <h1>News about ths <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.98 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+
     </>
   )
 }
+
+
