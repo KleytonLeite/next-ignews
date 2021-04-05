d67ba8a3a21b07b7204e0b51ec0e75a9bde56cbf
KleytonLeite <musicpteclados@gmail.com>
2021-04-05 10:53:50 -0300

    feat: Componente Button Subscribe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 
+import { SubscribeButton } from '../components/SubcribeButton';
 import styles from './home.module.scss';
 
 export default function Home() {
@@ -11,13 +12,14 @@ export default function Home() {
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
           <span>👏Hey, welcome</span>
-          <h1>News about ths <span>React</span> world.</h1>
+          <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
             <span>for $9.98 month</span>
           </p>
+          <SubscribeButton />
         </section>
-        <img src="/images/avatar.svg" alt="Girl coding" />
+        <img src="/images/avatar.svg" alt="Girl coding" />  
       </main>
 
     </>
