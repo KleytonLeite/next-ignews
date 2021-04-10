723fd721c9a221aab3b102eaef0c52b7393e378d
KleytonLeite <musicpteclados@gmail.com>
2021-04-10 15:57:44 -0300

    feat: Consumindo API do Prismic

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,5 +1,8 @@
+import Prismic from '@prismicio/client';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 
+import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss';
 
 export default function Posts() {
@@ -35,4 +38,21 @@ export default function Posts() {
             </main>
         </>
     );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    const prismic = getPrismicClient()
+
+    const response = await prismic.query([
+        Prismic.predicates.at('document.type', 'post')
+    ],{
+        fetch: ['post.title', 'post.content'],
+        pageSize: 100,
+    })
+
+    console.log(JSON.stringify(response, null, 2))
+
+    return {
+        props: {}
+    }
 }
