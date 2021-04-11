d5d7276d5dd47bb5a4a36b7bff80eece45d3c927
KleytonLeite <musicpteclados@gmail.com>
2021-04-11 01:34:31 -0300

    feat: Validando assinatura ativa

--- src/components/SubcribeButton/index.tsx
+++ src/components/SubcribeButton/index.tsx
@@ -1,4 +1,5 @@
 import { signIn, useSession } from 'next-auth/client';
+import router, { useRouter } from 'next/router';
 
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
@@ -10,9 +11,16 @@ interface SubcribeButtonProps {
 
 export function SubscribeButton({ priceId }: SubcribeButtonProps) {
     const [session] = useSession()
+    const router = useRouter()
+
     async function handleSubscribe() {
         if(!session) {
             signIn('github')
+            return;
+        }
+
+        if(session.activeSubscription) {
+            router.push('/posts')
             return;
         }
 
